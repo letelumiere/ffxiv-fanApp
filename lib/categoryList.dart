d5113006961ab5545de345c6659e456d61b61088
letelumiere <letelumiere@gmail.com>
2024-07-03 20:35:25 +0900

    first commit

---
+++ lib/categoryList.dart
@@ -0,0 +1,35 @@
+
+
+enum itemCategory {
+  Arms,
+  Tools,
+  Armor,
+  Accessories,
+  MedicineMeals,
+  Materials,
+  Other
+}
+
+List<String> menuCategoryList = [
+  "items"
+];
+
+List<String> itemCategory2List = [
+  "Arms",
+  "Tools",
+  "Armor",
+  "Accessories",
+  "Medicine & Meals",
+  "Materials",
+  "Other"
+];
+
+Map<String, List<String>> itemCategory3Map = {
+  "Arms" : ["A","B","C"],
+  "Tools" : ["A","B"],
+  "Armor" : ["A","B","C"],
+  "Accessories" : ["DD"],
+  "Medicine & Meals" : ["FF"],
+  "Materials" : ["CC"],
+  "Other" : ["EX"],
+};
